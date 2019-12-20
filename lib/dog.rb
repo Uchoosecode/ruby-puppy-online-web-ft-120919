da5bbb2964dd3aa335f40ce7d14417577f4fae05
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-20 17:11:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+
+
+class Dog 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+    
+  end
+    
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    self.all.each do |person|
+      puts "#{person.name}"
+    end
+  end 
+
+  def save
+    # self.class.all << self.name
+  end
+  
+  
+end
